8caaee2fd47fff01f4d63e6151f175619536e7ed
kamildyla <kamil.dyla09@gmail.com>
2020-11-30 21:19:40 +0100

    project maven

--- src/test/java/com/bottles/BottlesApplicationTests.java
+++ src/test/java/com/bottles/BottlesApplicationTests.java
@@ -1,13 +1,13 @@
-package com.bottles;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BottlesApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.bottles;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BottlesApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
